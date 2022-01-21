2a33c3a57650284412e7a381e2f66796a51a4494
unknown <matze85eikholts@gmail.com>
2022-01-21 08:18:40 +0300

    first lessons of functional programming in java

---
+++ lambdaExpressions/src/com/mylearn/Main.java
@@ -0,0 +1,15 @@
+package com.mylearn;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Function<Integer, Integer> plusOne = (x) -> x + 1;
+        System.out.println ("testing of assign lambda to functional interface " + plusOne.apply(5));
+        BiFunction<Integer, Integer, Integer> addTwoNumbers = (x, y) -> x + y;
+        Integer z = addTwoNumbers.apply(2, 3);
+        System.out.println("Sum check = " + z);
+    }
+}
